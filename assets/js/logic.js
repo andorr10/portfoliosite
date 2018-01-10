0e3d08ef9fdd8af731d532826db96d1d77b79aec
Andrew Orr <andreworr@Andrews-MacBook-Pro.local>
2018-01-10 16:37:15 -0600

    projects done

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,7 +4,29 @@ $( document ).ready(function() {
 	// AOS Transition Time
 	AOS.init({
 	  duration: 1200,
-	})
+	});
 
 
 }); //end document ready
+
+
+
+//------------------------------  Modal Logic   ------------------------------------//
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
