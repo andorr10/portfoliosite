e213a490cb3282f2dcbf47def1380a732d6bf468
Andrew Orr <andreworr@Andrews-MacBook-Pro.local>
2018-01-11 10:37:38 -0600

    modal working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,21 +12,21 @@ $( document ).ready(function() {
 
 
 //------------------------------  Modal Logic   ------------------------------------//
-var modal = document.getElementById('myModal');
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+// var modal = document.getElementById('myModal');
+// var btn = document.getElementById("myBtn");
+// var span = document.getElementsByClassName("close")[0];
 
 
-btn.onclick = function() {
-    modal.style.display = "block";
+document.getElementById("myID").onclick = function() {
+    document.getElementById('myModal').style.display = "block";
 };
 
-span.onclick = function() {
-    modal.style.display = "none";
+document.getElementsByClassName("close")[0].onclick = function() {
+    document.getElementById('myModal').style.display = "none";
 };
 
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        document.getElementById('myModal').style.display = "none";
     }
 };
